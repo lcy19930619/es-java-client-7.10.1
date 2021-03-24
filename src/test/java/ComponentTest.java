cd042bc283d06a25335029bae8e02022af4347b8
冷春阳 <lengchunyang@qianlima.com>
2021-03-24 17:28:24 +0800

    单元测试

--- src/test/java/ComponentTest.java
+++ src/test/java/ComponentTest.java
@@ -1,8 +1,17 @@
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONObject;
 import net.jlxxw.client.component.ElasticsearchComponent;
+import net.jlxxw.client.dto.AnalyzerDTO;
 import net.jlxxw.client.dto.IndexDTO;
 import net.jlxxw.client.holder.ElasticsearchClientHolder;
+import net.jlxxw.client.vo.SearchResult;
+import org.elasticsearch.index.query.QueryBuilders;
+import org.elasticsearch.search.builder.SearchSourceBuilder;
 import org.junit.Before;
 import org.junit.Test;
+
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * @author chunyang.leng
@@ -14,11 +23,38 @@ public class ComponentTest {
     public void before(){
         elasticsearchComponent = ElasticsearchClientHolder.getElasticsearchComponent();
     }
-
+    String indexName = "test111";
     @Test
     public void createIndex(){
+        elasticsearchComponent.deleteIndex(indexName);
         IndexDTO indexDTO = new IndexDTO();
-        indexDTO.setIndexName("test111");
+        indexDTO.setIndexName(indexName);
+        Map<String, AnalyzerDTO> properties = new HashMap<>();
+        AnalyzerDTO nameProperties = new AnalyzerDTO();
+        nameProperties.setType("text");
+        properties.put("name",nameProperties);
+        AnalyzerDTO addressProperties = new AnalyzerDTO();
+        addressProperties.setType("text");
+        properties.put("address",addressProperties);
+        indexDTO.setProperties(properties);
         elasticsearchComponent.createIndex(indexDTO);
     }
+
+    @Test
+    public void insertTest(){
+        JSONObject jsonObject = new JSONObject();
+        jsonObject.put("name","testName");
+        jsonObject.put("address","testAddress");
+        elasticsearchComponent.insertRecord(jsonObject.toJSONString(),indexName);
+    }
+
+    @Test
+    public void selectTest(){
+        SearchSourceBuilder searchSourceBuilder = new SearchSourceBuilder();
+        searchSourceBuilder.query(QueryBuilders.matchQuery("name","testName"));
+        SearchResult<JSONObject> objectSearchResult = elasticsearchComponent.queryCondition(indexName, searchSourceBuilder);
+        System.out.println(JSON.toJSONString(objectSearchResult));
+    }
+
+
 }
